4f8e644a27b86e4e6d85875722e1fb90f7846853
wonder0112 <hwhgdcn@gmail.com>
2019-04-04 10:08:44 +0800

    创建项目

---
+++ app/src/main/java/cn/edu/gdpt/topline171000hwh/SplashActivity.java
@@ -0,0 +1,29 @@
+package cn.edu.gdpt.topline171000hwh;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        //显示3秒，然后自动跳转到MainActivity.java
+        //定时器和任务
+        Timer timer=new Timer();
+        TimerTask task=new TimerTask() {//实质是子线程，重写Runnable()
+            @Override
+            public void run() {//重写run()
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        timer.schedule(task,3000);
+    }
+}
